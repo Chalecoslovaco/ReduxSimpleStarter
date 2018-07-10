8df8daa39c56ef989738877c932ea4b65702bed3
Sergio <sergiogarher@gmail.com>
2018-07-10 18:24:09 +0200

    Create functional component SearchBar + import SearchBar in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search-bar';
+
 const API_KEY = 'AIzaSyDkx3SsRxZO4t2lQOrWwMBKShA0inux0pA';
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
+}
+
+ReactDom.render(<App />, document.querySelector('.container'));
