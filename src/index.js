4cd7340e004962923f2c8eee315152efa54b944b
Sergio <sergiogarher@gmail.com>
2018-07-11 19:25:50 +0200

    Add VideoDetail component with embeded youtube video + null video prop handle

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
 import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 const API_KEY = 'AIzaSyDkx3SsRxZO4t2lQOrWwMBKShA0inux0pA';
 
 
@@ -19,6 +20,7 @@ class App extends Component{
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         )
