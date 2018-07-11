6c383ea482d572fa40c7bf9fd9ec5758b83aad3a
Sergio <sergiogarher@gmail.com>
2018-07-11 18:25:28 +0200

    Change App to class based component + add constructor + ytsearch function + state to store videos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
-
 const API_KEY = 'AIzaSyDkx3SsRxZO4t2lQOrWwMBKShA0inux0pA';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+
+class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, videos => { this.setState({ videos })}); //before ES6 -> this.setState({ videos: videos })
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
